c004864586604b9214762f5567a01ab632e28a79
Darin Doria <ddoria921@gmail.com>
2020-02-15 13:43:35 -0500

    Initial Commit

---
+++ ProjectX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectX
+//
+//  Created by Darin Doria on 2/15/20.
+//  Copyright © 2020 Darin Doria. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
